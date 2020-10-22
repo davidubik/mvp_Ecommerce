d7549fb8686bf4f07ce677e316f2f9089c1c60f8
davidubik <dtwebdev@outlook.fr>
2020-10-22 19:32:35 +0200

    Correction du bug et affichage des img par l'Api ok.

--- front/js/script.js
+++ front/js/script.js
@@ -1,51 +1,28 @@
-
-// const parentSection1 = document.querySelector('.section1');
-// const img1 = new Image();
-// const img2 = new Image();
-// const img3 = new Image();
-// const img4 = new Image();
-// const img5 = new Image();
-
-// // img1.classList.add('img');
-// // img2.classList.add('img');
-// // img3.classList.add('img');
-// // img4.classList.add('img');
-// // img5.classList.add('img');
-
-// // img1.src = "../back/images/vcam_1.jpg";
-// // img2.src ="../back/images/vcam_2.jpg"
-// // img3.src ="../back/images/vcam_3.jpg"
-// // img4.src ="../back/images/vcam_4.jpg"
-// // img5.src ="../back/images/vcam_5.jpg"
-
-// parentSection1.appendChild(img1);
-// parentSection1.appendChild(img2);
-// parentSection1.appendChild(img3);
-// parentSection1.appendChild(img4);
-// parentSection1.appendChild(img5);
-
 window.onload = function(){
 
     const req = new XMLHttpRequest();
      const method = "GET";
-    const url = "​http://localhost:3000/api/cameras";
+    const Url = "http://localhost:3000/api/cameras";
     
-    req.open("GET", url);
+    req.open("GET", Url);
     
     req.onreadystatechange = function(e){
         if(this.readyState === XMLHttpRequest.DONE){
             if(this.status === 200){
                 const response = (JSON.parse(this.responseText));
-                for(i = 0; i < image.length; i++){
-                    console.log(response[i].url);
+                for(i = 0; i < response.length; i++){
+                    console.log(response[i].imageUrl);
                     const image = document.createElement('img');
-                    image.src = response[i].url;
+                    image.src = response[i].imageUrl;
                     document.body.appendChild(image);
                     console.log(response[i]);
                 }
+                // console.log(response[i]);
+                
             } else{
                 console.log("Statut: " + this.status);
             }
+            
         }
     }
     
