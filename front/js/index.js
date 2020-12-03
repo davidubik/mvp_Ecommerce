4e5ac2d4f7273b77efec50eef792de165558b611
davidubik <dtwebdev@outlook.fr>
2020-12-03 17:44:01 +0100

    chagement du nom de la page.

---
+++ front/js/index.js
@@ -0,0 +1,69 @@
+window.onload = () => {
+
+    const req = new XMLHttpRequest();
+    const Url = "http://localhost:3000/api/cameras";
+    
+    req.open("GET", Url);
+    
+    req.onreadystatechange = function(e){
+        if(this.readyState === XMLHttpRequest.DONE){
+            if(this.status === 200){
+                const response = JSON.parse(this.responseText);
+                for(i = 0; i < response.length; i++){
+                    console.log(response[i].imageUrl);
+
+                    const camera = document.createElement("div");
+                    camera.classList.add("camera");
+
+                    const model = document.createElement("h1");
+                    model.textContent = response[i].name;
+                    
+                    
+                                  
+                     const link = document.createElement("a");
+                     link.textContent = "Plus d'info ici";
+                     link.style.color = '#000';
+                     link.href = './pages/produit.html?id=' + response[i]._id;
+                     
+                
+
+                    const definition = document.createElement("p");
+                    definition.textContent = response[i].description;
+
+                    const prix = document.createElement("p");
+                    prix.textContent = response[i].price + "€";
+
+                    const image = document.createElement("img");
+                    image.src = response[i].imageUrl;
+                    
+                    
+                    camera.appendChild(image);
+                    camera.appendChild(model);
+                    camera.appendChild(definition);
+                    camera.appendChild(prix);
+                    camera.appendChild(link);
+
+                   document.querySelector(".items-container").appendChild(camera);
+                   
+                    console.log(response[i]);
+                    console.log(response[i].name);
+                    
+                }
+            
+            } else{
+                console.error("Statut: " + this.status);
+            }
+            
+        }
+    }
+    
+    req.send();
+
+
+
+ 
+}
+
+
+
+
