8899f71f32e532cf2dec135f3b27f676d58d1b80
davidubik <dtwebdev@outlook.fr>
2020-12-10 18:50:37 +0100

    Création de la variable params pour récupérer l'id des produits via l'interface URLSearchParams, puis on attribut l'id dans la variable paramsId.

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,55 @@
-window.onload = function(){
+window.onload = () => {
 
+
+// Récupération de l URL (querystring) 
+  let params = new URLSearchParams (window.location.search);
+  
+    console.log(params.get('id'));
+
+
+
+  // Récupération du paramètre ID
+
+  let paramsId = params.get('id');
+
+  console.log(paramsId)
+
+
+// Récupération (parse des paramètres) 
+
+
+  // Construction de URL de API
+  // Requête API (XMLHttpRequest) 
+  // Injection dans le HTML
    
+
+    // const req = new XMLHttpRequest();
+    // 
+
+    // req.open("GET", Url);
+    
+    // req.onreadystatechange = function(e){
+
+    //     if(this.readyState === XMLHttpRequest.DONE && this.status === 200){
+    //         const response = JSON.parse(this.responseText);
+           
+
+    //        console.log(response);
+    //     }else{
+    //         console.error("Statut: " + this.status);
+    //     }
+
+    // }
+
+    
+   
+
+    // req.send();
+    // console.log(response);
+
+
+
 
 
 }
+
