fb06e6c517d974caaca3fe7ddbb9f901779b2843
davidubik <dtwebdev@outlook.fr>
2020-12-18 17:54:08 +0100

    Récupération de la réponse et affichage des éléments dans le html de la page produit.html

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,9 @@
 window.onload = () => {
 
 
+
+
+
 // Récupération de l URL (querystring) 
   let params = new URLSearchParams (window.location.search);
   
@@ -20,7 +23,7 @@ window.onload = () => {
     
   // Construction de URL de API
 
-  const Url = "http://localhost:3000/api/cameras" + paramsId;
+  const Url = "http://localhost:3000/api/cameras/" + paramsId;
 
   console.log(Url);
 
@@ -33,19 +36,49 @@ window.onload = () => {
   req.onreadystatechange = function(e){
 
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-      
-      
+      const response = JSON.parse(this.responseText);
+      console.log(Url);
 
 
-    }else{
-      console.error("Statut: " + this.status);    // Si une erreur avec la requête  
+        const productContainer = document.querySelector('.product_container');
+        const lentille1 = document.createElement('p');
+        const lentille2 = document.createElement('p');
+        const iD = document.createElement('p');
+        const name = document.createElement('h2');
+        const description = document.createElement('p');
+        const price = document.createElement('p');
+        const imgUrl = document.createElement('img');
+        
+        lentille1.innerHTML = response.lenses[0];
+        lentille2.innerHTML = response.lenses[1];
+        iD.innerHTML = response._id;
+        name.innerHTML = response.name;
+        description.innerHTML = response.description;
+        price.innerHTML = response.price + '€';
+        imgUrl.src = response.imageUrl;
+
+        productContainer.appendChild(name);
+        productContainer.appendChild(imgUrl);
+        productContainer.appendChild(lentille1);
+        productContainer.appendChild(lentille2)
+        productContainer.appendChild(iD);
+        productContainer.appendChild(price);
+       
+
+        
+
+      
+
+      }else{
+        console.error("Statut: " + this.status);    // Si une erreur avec la requête  
+    }
+
   }
 
-  }
-
+  req.send();
   // Injection dans le HTML
    
-
+  
     
 
 
