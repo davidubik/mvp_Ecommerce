aca3291c7e471044f728397d0076ddb24b3120c1
davidubik <dtwebdev@outlook.fr>
2021-03-04 19:13:37 +0100

    Supression des variables inutiles, travail sur le localStorage.

--- front/js/product.js
+++ front/js/product.js
@@ -3,10 +3,6 @@ window.onload = () => {
 
 // Récupération de l URL (querystring) 
   let params = new URLSearchParams (window.location.search);
-  
-
-
-
 
   // Récupération du paramètre ID
 
@@ -14,8 +10,6 @@ window.onload = () => {
 
   console.log(paramsId)
 
-
-    
   // Construction de URL de API
 
   const Url = "http://localhost:3000/api/cameras/" + paramsId;
@@ -31,77 +25,78 @@ window.onload = () => {
   req.onreadystatechange = function(e){
 
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      console.log(Url);
-
-
-         const productContainer = document.querySelector('.product-container');
-         const produitTitre = document.querySelector('.produit-titre').innerText = response.name;
-         const produitImg = document.querySelector('.produit-img').src = response.imageUrl;
-         const produitDescription = document.querySelector('.produit-description').innerText = response.description;
-         const produitId = document.querySelector('.produit-id').innerText = `Code article : ${response._id}`;
-         const produitPrix = document.querySelector('.produit-prix').innerText = `Prix: ${response.price}€`;
-         const select = document.querySelector('select');
-         const produitBouton = document.querySelector('.produit-bouton');
-        
-
-      //POUR CHAQUE LENTILLES JE CRÉER UNE BALISE OPTION
-
-
-        
-        
-        for(let i = 0; i < response.lenses.length; i++){
-
-          const option = document.createElement('option');
-          option.innerHTML = response.lenses[i];
-          select.appendChild(option);
+      const produit = JSON.parse(this.responseText);
       
+      // const productContainer = document.querySelector('.product-container');
+      document.querySelector('.produit-titre').innerText = produit.name;
+      document.querySelector('.produit-img').src = produit.imageUrl;
+      document.querySelector('.produit-description').innerText = produit.description;
+      document.querySelector('.produit-id').innerText = `Code article : ${produit._id}`;
+      document.querySelector('.produit-prix').innerText = `Prix: ${produit.price}€`;
+      const produitBouton = document.querySelector('.produit-bouton'); 
 
-       }        
+      //Récupération de la balise select
+      const select = document.querySelector('select');
+
+      //POUR CHAQUE LENTILLES JE CRÉER UNE BALISE OPTION   
+        
+        for(let i = 0; i < produit.lenses.length; i++){
+          
+            const option = document.createElement('option');
+            option.innerHTML = produit.lenses[i];
+            select.appendChild(option);
+
+       }    
+    
+  
 
         produitBouton.addEventListener('click', (e)=>{
-         
-         window.location.href='panier.html';
-         const objetResponse = response;
+
+            const modal = document.getElementById("myModal");
+
+            // Get the button that opens the modal
+            const btn = document.getElementById("myBtn");
+
+            // Get the <span> element that closes the modal
+            const span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks on the button, open the modal
+            btn.onclick = function() {
+              modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+              modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+              window.onclick = function(event) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+              }
+            } 
+            //LocalStorage
+            //On récupére le contenue de du produit
+            let panier = localStorage.getItem('panier') ? JSON.parse(localStorage.getItem('panier'))  :  []; 
+            //On ajoute le produit au panier
+            panier.push({
+              product : produit,
+              selectLense : select.value
+            });
+            //On sauvegar dans le localStorage
+            localStorage.setItem('panier', JSON.stringify(panier));
+
+              // localStorage.setItem('nameOfItem', produit);
+              // window.location.href='panier.html';
+            })
           
-        let obj = JSON.stringify(objetResponse);
-        localStorage.setItem("obj", obj);
-
-        const modal = document.getElementById("myModal");
-
-        // Get the button that opens the modal
-        const btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        const span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks on the button, open the modal
-        btn.onclick = function() {
-          modal.style.display = "block";
+          }else{
+            console.error("Statut: " + this.status);    // Si une erreur avec la requête  
         }
 
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-          modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-          if (event.target == modal) {
-            modal.style.display = "none";
-          }
-        } 
-        window.location.href='panier.html';
-       
-          console.log(obj);
-        })
+      }
       
-      }else{
-        console.error("Statut: " + this.status);    // Si une erreur avec la requête  
-    }
-
-  }
-   
   req.send();
   // Injection dans le HTML
    
