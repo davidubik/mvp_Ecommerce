44c9cc99e6ab80b45e403974e71c93f6fcbfacff
davidubik <dtwebdev@outlook.fr>
2021-05-23 20:04:49 +0200

    Mis en place de la methode Fetch pour les requêtes serveurs, à la place de XHR.

--- front/js/index.js
+++ front/js/index.js
@@ -1,67 +1,50 @@
 window.onload = () => {
 
-    const req = new XMLHttpRequest(); // Requête XML
-    const Url = "http://localhost:3000/api/cameras"; // Url de l'API où l'on envoie la requête
-    
-    req.open("GET", Url); // Instance la requête XML avec comme arguments le methode GET et l'URL.
-    
-
-    req.onreadystatechange = function(e){                       // AJout de l'EventHandler sur la requête
-    if(this.readyState === XMLHttpRequest.DONE){                // Si le raquête est finie
-            if(this.status === 200){                            // Si la requête excécuter et que le satue est === à 200 (OK)
-                const response = JSON.parse(this.responseText); // Traduit la réponse du Server au format JSON
-                for(i = 0; i < response.length; i++){           // On parcour la réponse du Server
-                    console.log(response[i].imageUrl);
-
-
-                    const camera = document.createElement("div");   // Création de l'élément camera (div) 
-                    camera.classList.add("camera");
-
-                    const txtContainer = document.createElement('div');
-                    txtContainer.classList.add('txt-container');
-
-                    const itemTitre = document.createElement("h3");     // Création du l'élément H1 on lui attribue le .name de la réponse Server
-                    itemTitre.textContent = response[i].name;
-                                
-                      const itemLien = document.createElement("a");      // Création de l'élément a qui affiche l'.id de la réponse Server et on utilise son href pour passé sur la page prduit.html
-                      itemLien.textContent = "En savoir plus";
-                      itemLien.style.color = '#000';
-                      itemLien.href = './pages/produit.html?id=' + response[i]._id;
-                     
-
-                    const itemPrix = document.createElement("p");                   // Création du l'élément P on lui attribue le .prix de la réponse Server
-                    itemPrix.textContent = `Prix: ${response[i].price}€`;
-
-                    const itemImage = document.createElement("img");        // Création du l'élément IMG on lui attribue la .imageUrl de la réponse Server
-                    itemImage.src = response[i].imageUrl;
-                    
-                    
-                    txtContainer.appendChild(itemTitre);
-                    camera.appendChild(itemImage);                          // On attache les variables à la balise Camera
-                    txtContainer.appendChild(itemPrix);
-                    txtContainer.appendChild(itemLien);
-
-                    document.querySelector(".items-container").appendChild(camera);  // Injection de CAMERA dans le HTML 
-                    document.querySelector(".items-container").appendChild(txtContainer);
-
-                    console.log(response[i]);
-                    console.log(response[i].name);
-                    
-                }
-            
-            } else{
-                console.error("Statut: " + this.status);    // Si une erreur avec la requête  
-            }
-            
-        }
-    }
-    
-    req.send(); // Envoy de la Requête au Server
-
-
-
+    //FETCH
+    fetch("http://localhost:3000/api/cameras")
+    .then((response) => response.json())
+    .then((response) => {
  
-}
+        //Récupération des élements depuis la réponse du server            
+        for(i = 0; i < response.length; i++){      
+        console.log(response[i].imageUrl);
+
+        //Injection des élémnents dans le HTML
+        const camera = document.createElement("div");   
+        camera.classList.add("camera");
+        document.querySelector(".items-container").appendChild(camera);
+
+       
+        const itemTitre = document.createElement("h3"); 
+        itemTitre.textContent = response[i].name;
+        
+
+         const txtContainer = document.createElement('div');
+        txtContainer.classList.add('txt-container');
+        txtContainer.append(itemTitre);
+        const itemImage = document.createElement("img");       
+        itemImage.src = response[i].imageUrl;
+        camera.append(itemImage);
+
+        const itemLien = document.createElement("a");      
+        itemLien.textContent = "En savoir plus";
+        itemLien.style.color = '#000';
+        itemLien.href = './pages/produit.html?id=' + response[i]._id; 
+
+        const itemPrix = document.createElement("p");                  
+        itemPrix.textContent = `Prix: ${response[i].price}€`;
+                                                            
+        txtContainer.appendChild(itemPrix);
+        txtContainer.appendChild(itemLien);
+                           
+        document.querySelector(".items-container").appendChild(txtContainer);
+        
+    }
+    console.log(response)
+})
+    .catch(error => console.log('Error : ' + error));
+ 
+    }
 
 
 
--- front/js/product.js
+++ front/js/product.js
@@ -5,105 +5,49 @@ window.onload = () => {
   let params = new URLSearchParams (window.location.search);
 
   // Récupération du paramètre ID
-
   let paramsId = params.get('id');
-
   console.log(paramsId)
 
-  // Construction de URL de API
 
-  const Url = "http://localhost:3000/api/cameras/" + paramsId;
+  fetch("http://localhost:3000/api/cameras/" + paramsId)
+  .then((produit) => produit.json())
+  .then((produit) => {
 
-  console.log(Url);
+    document.querySelector('.produit-titre').innerText = produit.name;
+    document.querySelector('.produit-img').src = produit.imageUrl;
+    document.querySelector('.produit-description').innerText = produit.description;
+    document.querySelector('.produit-id').innerText = `Code article : ${produit._id}`;
+    document.querySelector('.produit-prix').innerText = `Prix: ${produit.price}€`;
+    const produitBouton = document.querySelector('.produit-bouton'); 
 
-  // Requête API (XMLHttpRequest) 
-
-  const req = new XMLHttpRequest;
-
-  req.open("GET", Url);
-
-  req.onreadystatechange = function(e){
-
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-      const produit = JSON.parse(this.responseText);
-      
-      // const productContainer = document.querySelector('.product-container');
-      document.querySelector('.produit-titre').innerText = produit.name;
-      document.querySelector('.produit-img').src = produit.imageUrl;
-      document.querySelector('.produit-description').innerText = produit.description;
-      document.querySelector('.produit-id').innerText = `Code article : ${produit._id}`;
-      document.querySelector('.produit-prix').innerText = `Prix: ${produit.price}€`;
-      const produitBouton = document.querySelector('.produit-bouton'); 
-
-      //Récupération de la balise select
+     //Récupération de la balise select
       const select = document.querySelector('select');
 
-      //POUR CHAQUE LENTILLES JE CRÉER UNE BALISE OPTION   
-        
+      //Lentilles création d'un balise option.
         for(let i = 0; i < produit.lenses.length; i++){
-          
-            const option = document.createElement('option');
-            option.innerHTML = produit.lenses[i];
-            select.appendChild(option);
-
-       }    
-    
-  
-
-        produitBouton.addEventListener('click', (e)=>{
-
-            const modal = document.getElementById("myModal");
-
-            // Get the button that opens the modal
-            const btn = document.getElementById("myBtn");
-
-            // Get the <span> element that closes the modal
-            const span = document.getElementsByClassName("close")[0];
-
-            // When the user clicks on the button, open the modal
-            btn.onclick = function() {
-              modal.style.display = "block";
+             const option = document.createElement('option');
+             option.innerHTML = produit.lenses[i];
+             select.appendChild(option);
             }
-
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
-              modal.style.display = "none";
-            }
-
-            // When the user clicks anywhere outside of the modal, close it
-              window.onclick = function(event) {
-              if (event.target == modal) {
-                modal.style.display = "none";
-              }
-            } 
-            //LocalStorage
+            
+            //EventListener sur le Bouton et LocalStorage
+            produitBouton.addEventListener('click', (e)=>{
+             
             //On récupére le contenue de du produit
-            let panier = localStorage.getItem('panier') ? JSON.parse(localStorage.getItem('panier'))  :  []; 
-            //On ajoute le produit au panier
-            panier.push({
-              product : produit,
-              selectLense : select.value
-            });
-            //On sauvegar dans le localStorage
-            localStorage.setItem('panier', JSON.stringify(panier));
+             let panier = localStorage.getItem('panier') ? JSON.parse(localStorage.getItem('panier'))  :  []; 
+             
+             //On ajoute le produit au panier
+             panier.push({ product : produit, selectLense : select.value});
+           
+             //On sauvegar dans le localStorage
+              localStorage.setItem('panier', JSON.stringify(panier));
+              console.log(panier);
+              window.location.href='panier.html';
 
-              // localStorage.setItem('nameOfItem', produit);
-              // window.location.href='panier.html';
             })
-          
-          }else{
-            console.error("Statut: " + this.status);    // Si une erreur avec la requête  
-        }
-
-      }
-      
-  req.send();
-  // Injection dans le HTML
-   
-  /* À faire Ajouter le(s) produit(s) à son panier, et stylisé la page */
-    
-
-/* Créer le bouton panier sur lequel on place un eventListener avec callbacks et un localStorage */
+     console.log(produit);
+  })
+  .catch(error => console.log('Error : ' + error));
 
 }
 
