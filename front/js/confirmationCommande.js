8df65276a852b108406a799e190f12bf1dbff336
davidubik <dtwebdev@outlook.fr>
2021-05-29 20:47:21 +0200

    Mis en place du UrlSearchParams.

--- front/js/confirmationCommande.js
+++ front/js/confirmationCommande.js
@@ -1,6 +1,14 @@
 window.onload = () => {
 
 
+   let params = new URLSearchParams (window.location.search);
+
+    let paramId = params.get('id');
+    let paramTotal = params.get('total');
+    console.log(paramId);
+    console.log(paramTotal);
+
+    
 
     
 }
